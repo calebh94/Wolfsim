3f9af11fabba3812f7ff1f0e7e2bda9757e82ede
charris92 <charris92@gatech.edu>
2021-03-26 10:01:17 -0400

    wolf eating, roaming, and then dying from bear attack

--- src/wolfsim.py
+++ src/wolfsim.py
@@ -7,21 +7,47 @@ GT M&S Spring 2021
 '''
 
 import simpy
+#TODO: test with agentpy as ap
 
 
-def wolf(env):
-    while True:
-        print("Start eating at %d" % env.now)
-        eating_duration = 5
-        yield env.timeout(eating_duration)
+class Wolf(object):
+    def __init__(self, env):
+        self.env = env
+        self.action = env.process(self.run())
+        self.alive = True
+        self.position = [0,0]
+        self.speed = 5
 
-        print("Start roaming at %d" % env.now)
-        roaming_duration = 20
-        yield env.timeout(roaming_duration)
+    def run(self):
+        while self.alive:
+            print("Start eating at %d" % self.env.now)
+            eating_duration = 5
+            # yield env.timeout(eating_duration)\
+            try:
+                yield self.env.process(self.eat(eating_duration))
+            except simpy.Interrupt:
+                print("Sim interrupted. Wolf tracking may have failed or wolf has become deceased")
+                self.alive = False
+
+            print("Start roaming at %d" % env.now)
+            roaming_duration = 20
+            # yield env.timeout(roaming_duration)
+            yield self.env.timeout(roaming_duration)
+
+    def eat(self, duration):
+        yield self.env.timeout(duration)
+
+
+def bear(env, wolf):
+    yield env.timeout(30)
+    wolf.action.interrupt()
+
 
 env = simpy.Environment()
-env.process(wolf(env))
-env.run(until=500)
+wolf = Wolf(env)
+env.process(bear(env, wolf))
+env.run(until=200)
+
 
 
 

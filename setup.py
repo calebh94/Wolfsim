a573d3cedf31e46d847d8798debadb55b62f5dfb
charris92 <charris92@gatech.edu>
2021-03-26 15:34:46 -0400

    Agent based testing using agentpy

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Wolfsim',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='harri',
+    author_email='',
+    description=''
+)
